e348e7a934e9c8b85193e42ca0d334cd405b4816
leandroofaria <leandrogabrielfaria05@gmail.com>
2024-11-05 12:40:27 -0300

    Criando useState

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,21 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import { FaGithub, FaPlus } from 'react-icons/fa';
 import { Container, Header, Form, SubmitButton } from './styles';
 
 export default function Main() {
+
+    const [newRepo, setNewRepo] = useState('');
+
+    function handleinputChange(event){
+        setNewRepo(event.target.value)
+    }
+
     return (
         <Container>
+
             <Header>
+
                 <FaGithub size={25} />
                 <h1>Meus Repositórios</h1>
+
             </Header>
 
             <Form onSubmit={() => {}}>
-                <input type="text" placeholder="Adicionar Repositorios" />
+
+                <input type="text" placeholder="Adicionar Repositorios" value={newRepo} onChange={handleinputChange}/>
+
                 <SubmitButton>
                     <FaPlus color="white" size={14} />
                 </SubmitButton>
+
             </Form>
+
         </Container>
     );
 }
