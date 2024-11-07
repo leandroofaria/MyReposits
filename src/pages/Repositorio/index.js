6546d372f9ebb076655bf487d04d7badfed68e69
leandroofaria <leandrogabrielfaria05@gmail.com>
2024-11-07 10:50:47 -0300

    Get Detalhes Repositorio

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { useParams } from "react-router-dom";
+import { Container } from './styles';
+import api from "../../services/api";
 
 export default function Repositorio() {
-    const { repositorio } = useParams(); 
+    const { paginaRepositorio } = useParams();  
 
+    const [repositorio, setRepositorio] = useState({})
+    const [issues, setIssues] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        async function load() {
+            const nomeRepo = decodeURIComponent(paginaRepositorio);
+            
+            const [repositorioData, issuesData] = await Promise.all([
+                api.get(`/repos/${nomeRepo}`),
+                api.get(`/repos/${nomeRepo}/issues`, {
+                    params: {
+                        state: 'open',
+                        per_page: 5
+                    }
+                })
+            ]);
+            
+            
+
+            setRepositorio(repositorioData.data)
+            setIssues(issuesData.data)
+            setLoading(false)
+        }
+
+        load();
+    }, [paginaRepositorio]); 
     return (
-        <h1>Repositorio <br />
-            {repositorio}
-        </h1>
+        <Container>
+            
+        </Container>
     );
-}
+};
