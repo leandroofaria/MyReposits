e9f8ae317f05bc1cbde771582a185a7bc78af577
leandroofaria <leandrogabrielfaria05@gmail.com>
2024-11-07 12:36:01 -0300

    Exibindo Detalhes

--- src/pages/Repositorio/styles.js
+++ src/pages/Repositorio/styles.js
@@ -1,13 +1,60 @@
 import styled from "styled-components";
+import {Link} from 'react-router-dom'
 
-export const Container = styled.div`
-    
+export const Loading = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
 `;
 
-// export const Container = styled.div`
+export const Container = styled.div`
+    max-width: 700px;
+    background: #494850;
+    border-radius: 4px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+    padding: 30px;
+    margin: 80px auto;
+`;
 
-// `;
+export const Owner = styled.header`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    
+    img {
+        width: 150px;
+        border-radius: 20%;
+        margin: 20px 0;
 
-// export const Container = styled.div`
+    }
+
+    h1 {
+        font-size: 30px;
+        color: whitesmoke;
+        margin-top: 0.8rem;
+        
+    }
+
+    p {
+        margin-top: 5px;
+        font-size: 14px;
+        color: whitesmoke;
+        text-align: center;
+        line-height: 1.4;
+        max-width: 400px;
+        padding: 7px 0;
+    }
+`;
+
+
+export const BackButton = styled(Link)`
+    border: 0;
+    outline: 0;
+    background: transparent;
+
+`;
+
+// export const Loading = styled.div`
 
 // `;
